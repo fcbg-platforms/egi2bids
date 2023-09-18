56d00bc21bd64e46cb516449a0cff9c8889d1d03
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2023-09-18 11:18:41 +0200

    update structure

--- egi2bids/mff2bids.py
+++ egi2bids/mff2bids.py
@@ -15,58 +15,19 @@ from mne_bids import (
     write_raw_bids,
 )
 
-from .utils._checks import _check_value, _ensure_path
+from .constants import _CH_NAMES_EGI
+from .utils._checks import check_value, ensure_path
 from .utils.logs import logger, verbose
 
-# fmt:off
-CH_NAMES_EGI = [
-    "1", "F8", "3", "4", "F2", "6", "7", "8",
-    "9", "AF8", "11", "AF4", "13", "14", "FCz", "16",
-    "17", "FP2", "19", "20", "Fz", "22", "23", "FC1",
-    "25", "FPz", "27", "28", "F1", "30", "31", "32",
-    "33", "AF3", "35", "F3", "FP1", "38", "39", "40",
-    "41", "FC3", "43", "C1", "45", "AF7", "F7", "F5",
-    "FC5", "50", "51", "52", "53", "54", "55", "56",
-    "57", "58", "C3", "60", "61", "FT7", "63", "C5",
-    "65", "CP3", "FT9", "T9", "T7", "70", "71", "72",
-    "73", "74", "75", "CP5", "77", "78", "CP1", "80",
-    "81", "82", "83", "TP7", "85", "P5", "P3", "P1",
-    "89", "CPz", "91", "92", "93", "TP9", "95", "P7",
-    "PO7", "98", "99", "100", "Pz", "102", "103", "104",
-    "105", "P9", "107", "108", "PO3", "110", "111", "112",
-    "113", "114", "115", "O1", "117", "118", "POz", "120",
-    "121", "122", "123", "124", "125", "Oz", "127", "128",
-    "129", "130", "131", "132", "133", "134", "135", "136",
-    "137", "138", "139", "PO4", "141", "P2", "CP2", "144",
-    "145", "146", "147", "148", "149", "O2", "151", "152",
-    "P4", "154", "155", "156", "157", "158", "159", "160",
-    "PO8", "P6", "163", "CP4", "165", "166", "167", "168",
-    "P10", "P8", "171", "CP6", "173", "174", "175", "176",
-    "177", "178", "TP8", "180", "181", "182", "C4", "184",
-    "C2", "186", "187", "188", "189", "TP10", "191", "192",
-    "193", "C6", "195", "196", "197", "198", "199", "200",
-    "201", "T8", "203", "204", "205", "FC4", "FC2", "208",
-    "209", "T10", "FT8", "212", "FC6", "214", "215", "216",
-    "217", "218", "FT10", "220", "221", "F6", "223", "F4",
-    "225", "F10", "227", "228", "229", "230", "231", "232",
-    "233", "234", "235", "236", "237", "238", "239", "240",
-    "241", "242", "243", "244", "245", "246", "247", "248",
-    "249", "250", "251", "F9", "253", "254", "255", "256",
-    "Cz",
-]
-# fmt: on
 
-
-def _extract_folder(
-    file: Union[str, Path], dir_: Union[str, Path] = None
-) -> Path:
+def _extract_folder(file: Union[str, Path], dir_: Union[str, Path] = None) -> Path:
     """Extract a .mff compressed folder to its original form."""
     # check paths and file extension
-    file = _ensure_path(file, must_exist=True)
+    file = ensure_path(file, must_exist=True)
     ext = file.suffix
-    _check_value(ext, (".tar", ".zip", ".mff"), "extension")
+    check_value(ext, (".tar", ".zip", ".mff"), "extension")
     dir_ = Path.cwd() if dir_ is None else dir_
-    dir_ = _ensure_path(dir_, must_exist=True)
+    dir_ = ensure_path(dir_, must_exist=True)
     # open the archive if needed
     archive_readers = {
         ".tar": tarfile.open,
@@ -112,7 +73,7 @@ def mff2bids(
         mff_source = _extract_folder(mff_source, dir_=wd)
 
         # BIDS root
-        bids_root = _ensure_path(bids_root, must_exist=False)
+        bids_root = ensure_path(bids_root, must_exist=False)
         eeg_bids_path = BIDSPath(
             root=bids_root,
             subject=subject,
@@ -130,9 +91,7 @@ def mff2bids(
             logger.info("Saving source data to %s", source_bids_root)
             source_bids_path = eeg_bids_path.copy()
             source_bids_path.update(root=source_bids_root)
-            source_bids_path = source_bids_path.fpath.with_suffix(
-                mff_source.suffix
-            )
+            source_bids_path = source_bids_path.fpath.with_suffix(mff_source.suffix)
             if source_bids_path.exists() and overwrite is False:
                 raise ValueError(
                     f"Cannot write source data. Source data {source_bids_path}"
@@ -148,7 +107,7 @@ def mff2bids(
         for i, ch in enumerate(raw.info["ch_names"]):
             if i > 256:
                 break
-            new_chs[ch] = CH_NAMES_EGI[i]
+            new_chs[ch] = _CH_NAMES_EGI[i]
         raw.rename_channels(new_chs)
 
         # find events in stim channel
